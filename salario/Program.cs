84dfa2152fdd776377ddcf9cbcc073e7065c6a9e
VictorLima <victoraugustolima1@gmail.com>
2022-09-26 21:45:01 -0300

    salario

---
+++ salario/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        Console.Clear();
+
+
+        string[] entrada = Console.ReadLine().Split();
+
+        double a = double.Parse(entrada[0]);
+        double b = double.Parse(entrada[1]);
+        double c = double.Parse(entrada[2]);
+
+    
+        Console.WriteLine($"Number = {a} Salario = {b*c}");
+
+    }
+}
